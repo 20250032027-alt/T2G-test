731d22ce8fbd55991bde198b4cb1ff10571656f9
20250032027-alt <20250032027@my.xu.edu.ph>
2026-06-09 17:50:11 +0800

    test product

--- assets/products-data.js
+++ assets/products-data.js
@@ -1,5 +1,5 @@
 /* ============================================================
-   products-data.js - exported from Dev Mode on 6/7/2026
+   products-data.js - exported from Dev Mode on 6/9/2026
    REPLACE the entire contents of assets/products-data.js with this.
    Then push to GitHub to make changes live on all browsers.
 ============================================================ */
@@ -7,48 +7,17 @@
 window.T2G_SHOPEE_URL = "https://shopee.ph/shop/1013182247";
 
 window.T2G_PRODUCTS_DEFAULT = {
-  "1": {
-    "id": "1",
-    "name": "TEST GOOD",
-    "price": 100,
-    "priceDisplay": "PHP 100.00",
+  "coco-sugar": {
+    "id": "coco-sugar",
+    "name": "Coco Sugar",
+    "price": 90,
+    "priceDisplay": "PHP 90.00",
     "priceRange": null,
     "variants": null,
-    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "description": "<p>A natural alternative to refined sugar</p><ul><li>Made from 100% fresh coconut sap.</li><li>Low GI of 35</li></ul>",
     "related": [],
     "imageName": "product.png",
-    "weightKg": 0.35,
-    "shopeeUrl": null
-  },
-  "2": {
-    "id": "2",
-    "name": "TEST GOOD 2",
-    "price": 160,
-    "priceDisplay": "PHP 160.00",
-    "priceRange": null,
-    "variants": null,
-    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
-    "related": [
-      "1"
-    ],
-    "imageName": "product1.png",
-    "weightKg": 0.5,
-    "shopeeUrl": null
-  },
-  "3": {
-    "id": "3",
-    "name": "TEST GOOD 2",
-    "price": 10,
-    "priceDisplay": "PHP 10.00",
-    "priceRange": null,
-    "variants": null,
-    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
-    "related": [
-      "1",
-      "2"
-    ],
-    "imageName": "product3.png",
-    "weightKg": 0.5,
+    "weightKg": 0.2,
     "shopeeUrl": null
   }
 };
