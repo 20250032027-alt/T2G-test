4b65d23de6010d02c88e1ee81a961f816a449d6d
20250032027-alt <20250032027@my.xu.edu.ph>
2026-06-20 12:59:47 +0800

    test push product

--- assets/products-data.js
+++ assets/products-data.js
@@ -1,5 +1,5 @@
 /* ============================================================
-   products-data.js - exported from Dev Mode on 6/9/2026
+   products-data.js - exported from Dev Mode on 6/20/2026
    REPLACE the entire contents of assets/products-data.js with this.
    Then push to GitHub to make changes live on all browsers.
 ============================================================ */
@@ -19,8 +19,265 @@ window.T2G_PRODUCTS_DEFAULT = {
     "imageName": "product.png",
     "weightKg": 0.2,
     "shopeeUrl": null
+  },
+  "adlai": {
+    "id": "adlai",
+    "name": "Adlai",
+    "price": 260,
+    "priceDisplay": "PHP 260.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "adlai 1.png",
+    "images": [
+      "adlai 1.png",
+      "adlai 2.png",
+      "adlai 3.png"
+    ],
+    "weightKg": 1,
+    "shopeeUrl": null
+  },
+  "aminos": {
+    "id": "aminos",
+    "name": "Aminos",
+    "price": 160,
+    "priceDisplay": "PHP 160.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "aminos 1.png",
+    "images": [
+      "aminos 1.png",
+      "aminos 2.png",
+      "aminos 3.png",
+      "aminos 4.png",
+      "aminos 5.png",
+      "amonos 2.png"
+    ],
+    "weightKg": 0.32,
+    "shopeeUrl": null
+  },
+  "balsamic": {
+    "id": "balsamic",
+    "name": "Balsamic",
+    "price": 210,
+    "priceDisplay": "PHP 210.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "balsami 3.png",
+    "images": [
+      "balsami 3.png",
+      "balsamic 1.png",
+      "balsamic 2. png"
+    ],
+    "weightKg": 0.38,
+    "shopeeUrl": null
+  },
+  "coco-glow": {
+    "id": "coco-glow",
+    "name": "Coco Glow",
+    "price": 160,
+    "priceDisplay": "PHP 160.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "coco glow 1.png",
+    "images": [
+      "coco glow 1.png",
+      "coco glow 2.png",
+      "coco glow 3.png"
+    ],
+    "weightKg": 0.1,
+    "shopeeUrl": null
+  },
+  "coco-sugar-sachet": {
+    "id": "coco-sugar-sachet",
+    "name": "Coco Sugar Sachet",
+    "price": 130,
+    "priceDisplay": "PHP 130.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "sachet 1.png",
+    "images": [
+      "sachet 1.png",
+      "sachet 2.png",
+      "sachet 3.png"
+    ],
+    "weightKg": 0.125,
+    "shopeeUrl": null
+  },
+  "cocoregano": {
+    "id": "cocoregano",
+    "name": "Cocoregano",
+    "price": 165,
+    "priceDisplay": "PHP 165.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "Cocoregano 1.png",
+    "images": [
+      "Cocoregano 1.png",
+      "cocoregano 2.png",
+      "cocoregano 3.png"
+    ],
+    "weightKg": 0.8,
+    "shopeeUrl": null
+  },
+  "cubes": {
+    "id": "cubes",
+    "name": "Cubes",
+    "price": 150,
+    "priceDisplay": "PHP 150.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "cubes 2.png",
+    "images": [
+      "cubes 2.png",
+      "cubes 1.png",
+      "cubes 3.png",
+      "CUBES 4.png"
+    ],
+    "weightKg": 0.17,
+    "shopeeUrl": null
+  },
+  "flour": {
+    "id": "flour",
+    "name": "Flour",
+    "price": 125,
+    "priceDisplay": "PHP 125.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "Flour 1.png",
+    "images": [
+      "Flour 1.png",
+      "Flour 2.png",
+      "flour 3.png"
+    ],
+    "weightKg": 0.5,
+    "shopeeUrl": null
+  },
+  "lip-balm": {
+    "id": "lip-balm",
+    "name": "Lip Balm",
+    "price": 80,
+    "priceDisplay": "PHP 80.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "lip balm 1.png",
+    "images": [
+      "lip balm 1.png",
+      "lip balm 2.png",
+      "lip balm 3.png"
+    ],
+    "weightKg": 0.17,
+    "shopeeUrl": null
+  },
+  "cocomint": {
+    "id": "cocomint",
+    "name": "Cocomint",
+    "price": 110,
+    "priceDisplay": "PHP 110.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "mint 1.png",
+    "images": [
+      "mint 1.png",
+      "mint 2.png",
+      "mint 3.png"
+    ],
+    "weightKg": 0.1,
+    "shopeeUrl": null
+  },
+  "coco-savor": {
+    "id": "coco-savor",
+    "name": "Coco Savor",
+    "price": 150,
+    "priceDisplay": "PHP 150.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "savor 1.png",
+    "images": [
+      "savor 1.png",
+      "savor 2.png",
+      "savor 3.png"
+    ],
+    "weightKg": 0.32,
+    "shopeeUrl": null
+  },
+  "cocorush-200g": {
+    "id": "cocorush-200g",
+    "name": "CocoRush 200g",
+    "price": 90,
+    "priceDisplay": "PHP 90.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "200 g 1.png",
+    "images": [
+      "200 g 1.png",
+      "200 G.png",
+      "200G 3.png"
+    ],
+    "weightKg": 0.2,
+    "shopeeUrl": null
+  },
+  "cocorush-500g": {
+    "id": "cocorush-500g",
+    "name": "CocoRush 500g",
+    "price": 190,
+    "priceDisplay": "PHP 190.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "500 g 3.png",
+    "images": [
+      "500 g 3.png",
+      "500 g 4.png",
+      "500g 1.png",
+      "500g 2.png"
+    ],
+    "weightKg": 0.5,
+    "shopeeUrl": null
+  },
+  "vco-essence": {
+    "id": "vco-essence",
+    "name": "VCO Essence",
+    "price": 160,
+    "priceDisplay": "PHP 160.00",
+    "priceRange": null,
+    "variants": null,
+    "description": "<p>Product description goes here.</p><ul><li>Feature one</li><li>Feature two</li></ul>",
+    "related": [],
+    "imageName": "VCO 1.png",
+    "images": [
+      "VCO 1.png",
+      "VCO 2.png",
+      "VCO 3.png"
+    ],
+    "weightKg": 0.15,
+    "shopeeUrl": null
   }
 };
 
-/* Live working copy - devmode.js will merge localStorage overrides on top f*/
+/* Live working copy - devmode.js will merge localStorage overrides on top */
 window.T2G_PRODUCTS = Object.assign({}, window.T2G_PRODUCTS_DEFAULT);
